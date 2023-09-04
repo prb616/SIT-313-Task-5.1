c1bb68b292f4749baa1385f712ebad239f72b68e
prb616 <127139401+prb616@users.noreply.github.com>
2023-09-05 02:07:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react';
+import './App.css';
+import CenteredBar from './Components/Bar';
+import OptionsSelector from './Components/OptionsSelector';
+
+
+function App() {
+ 
+
+  return (
+    
+    <div className="App">
+      <CenteredBar/>
+     
+      <OptionsSelector />
+    </div>
+  );
+}
+export default App;
